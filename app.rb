a5de81de5c75622bf10e648861be67a74aee0a00
lapeyrem19 <lapeyrem19@ashevilleschool.org>
2018-06-12 15:53:05 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Margot"
+end
+
+get'/hometown' do
+  "My hometown is new orleans"
+end
+
+get '/favorite-song' do
+"My favorite song is fire"
+end
+
 end
